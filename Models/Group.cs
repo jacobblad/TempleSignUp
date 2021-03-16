9434d8235b5be9821070cf5052647759e443bc1b
jacobblad <66271471+jacobblad@users.noreply.github.com>
2021-03-16 09:39:05 -0600

    Added Group model

---
+++ Models/Group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleSignUp.Models
+{
+    public class Group
+    {
+        [Key]
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
